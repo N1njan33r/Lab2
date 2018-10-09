5e791158eb441d77a5df22f74812f19aa950c690
Alexander Inman <fujiwara06@gmail.com>
2018-10-08 20:58:25 -0400

    Real initial

---
+++ Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perimeter Discovery Central");
+
+            string userInput;
+            decimal width, length, height;
+            bool retry = true;
+            bool inputblank = true;
+
+            while (retry)
+            {
+                Console.WriteLine("===Enter Width, Length, and Height===");
+                do
+                {
+                    Console.Write("Width (feet):");
+                    userInput = Console.ReadLine();
+
+                } while (inputblank);
+            }
+        }
+    }
+}
