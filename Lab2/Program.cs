c1bbc9f7d6fc38e7c1b18566051010539e9fdddf
Alexander Inman <fujiwara06@gmail.com>
2018-10-11 21:40:09 -0400

    Lab 2 complete. Will not accept strings, but that's okay because spec...

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -14,19 +14,65 @@ namespace Lab2
 
             string userInput;
             decimal width, length, height;
-            bool retry = true;
-            bool inputblank = true;
 
-            while (retry)
+            while (true)
             {
-                Console.WriteLine("===Enter Width, Length, and Height===");
-                do
-                {
-                    Console.Write("Width (feet):");
-                    userInput = Console.ReadLine();
+                Console.WriteLine("===Enter Width, Length, and Height in Feet===");
 
-                } while (inputblank);
+                Console.Write("Width:");
+                userInput = Console.ReadLine();
+                while (userInput == "")
+                {
+                    Console.Write("Width:");
+                    userInput = Console.ReadLine();
+                }
+
+                width = Convert.ToDecimal(userInput);
+                userInput = "";
+
+                Console.Write("Length:");
+                userInput = Console.ReadLine();
+                while (userInput == "")
+                {
+                    Console.Write("Length:");
+                    userInput = Console.ReadLine();
+                }
+
+                length = Convert.ToDecimal(userInput);
+                userInput = "";
+
+                Console.Write("Height (optional):");
+                userInput = Console.ReadLine();
+                if (userInput != "")
+                {
+                    height = Convert.ToDecimal(userInput);
+                }
+                else
+                {
+                    height = 0;
+                }
+
+                decimal area = width * length;
+                Console.WriteLine("Area = " + area);
+                decimal peri = 2 * width + 2 * length;
+                Console.WriteLine("Perimeter = " + peri);
+                if (height != 0)
+                {
+                    decimal volu = area * height;
+                    Console.WriteLine("Volume = " + volu);
+                }
+
+                Console.Write("Would you like to try again? (y/n):");
+                if (Console.ReadLine() == "y")
+                {
+                    continue;
+                }
+
+                break;
             }
+
+            Console.WriteLine("Thank you!");
+            Console.ReadKey();
         }
     }
 }
